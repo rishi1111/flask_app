38629460d71925dc25850f77ddf3ccd9b1afed2e
Rishi <rishikambil@gmail.com>
2019-02-03 15:15:08 +0530

    Creating and deleting blogs

--- app.py
+++ app.py
@@ -15,17 +15,42 @@ class Blogpost(db.Model):
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('index.html',posts = posts)
 
 @app.route('/add')
 def add():
     return render_template('add.html')
 
-@app.route('/addpost')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post=Blogpost.query.filter_by(id=post_id).one()
+    return render_template('post.html',post=post)
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    post=Blogpost.query.filter_by(id=post_id).one()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/addpost', methods=['POST'])
 def addpost():
-    pass
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 if __name__ =='__main__':
     app.run(debug=True)
